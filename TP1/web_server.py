c718b27eaf63f41dcd128d1e129e6932bde21979
Thomas LEGUERE <thomas.leguere@epita.fr>
2025-09-08 12:55:24 +0000

    feat(MLOps): add the uvicorn

--- TP1/web_server.py
+++ TP1/web_server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import joblib
+import uvicorn
 
 app = FastAPI()
 
@@ -12,3 +13,6 @@ async def read_root(size: int, nb_rooms: int, garden: bool):
     regression_model = joblib.load("regression.joblib")
     prediction = regression_model.predict([[size, nb_rooms, garden]])
     return {"y_pred": prediction[0]}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5027)
